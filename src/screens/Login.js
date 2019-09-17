0221c46faaefd09cd76c8b8fa067f93387934839
cog1to <sorryforbadname@gmail.com>
2019-09-17 21:48:32 +0300

    Fixed login layout for Android.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -21,11 +21,61 @@ import { saveAddress, loadSavedAddress } from '../redux/reducers/storage/actions
 import { connect as reduxConnect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
+import KeyboardState from '../components/common/KeyboardState'
+import MeasureLayout from '../components/common/MeasureLayout'
+
+// Safe area check.
+import { isIphoneX } from '../utils/IsIphoneX';
+
 // Themes.
 import ThemeManager from '../themes/ThemeManager'
 
 // Global error banner.
 import ErrorBanner from '../components/ErrorBanner'
+
+class KeyboardAwareLoginForm extends React.Component {
+    static propTypes = {
+        // From `KeyboardState`
+        containerHeight: PropTypes.number.isRequired,
+        contentHeight: PropTypes.number.isRequired,
+        keyboardHeight: PropTypes.number.isRequired,
+        keyboardVisible: PropTypes.bool.isRequired,
+        keyboardWillShow: PropTypes.bool.isRequired,
+        keyboardWillHide: PropTypes.bool.isRequired,
+        keyboardAnimationDuration: PropTypes.number.isRequired,
+
+        // Rendering content
+        children: PropTypes.node,
+    }
+
+    static defaultProps = {
+        children: null,
+    }
+
+    render() {
+        const {
+            children,
+            containerHeight,
+            contentHeight,
+            keyboardHeight,
+            keyboardVisible,
+            containerY
+        } = this.props
+
+        const useContentHeight = keyboardVisible
+
+        const containerStyle = Platform.OS === 'ios' 
+            ? { height: useContentHeight ? (contentHeight - (isIphoneX() ? 24 : 0)) : containerHeight, flex: 1 } 
+            : { flex: 1 }
+        
+        return (
+            <View style={containerStyle}>
+                {children}
+            </View>
+        )
+    }
+}
+
 
 class Login extends React.Component {
     state = {
@@ -90,39 +140,49 @@ class Login extends React.Component {
 
         return (
             <SafeAreaView style={styles.container}>
-                <KeyboardAvoidingView style={styles.credentialsContainer} behavior="padding">
-                    <Image
-                        source={require('../../assets/images/yamp_big_logo.png')}
-                        style={{resizeMode: 'contain', width: imageHeight, height: imageHeight }} />
-                    <Input
-                        style={styles.input}
-                        placeholderTextColor={placeholderTextColor}
-                        placeholder='Host'
-                        onChangeText={this.handleHostChange}
-                        value={host} />
-                    <Input
-                        style={styles.input}
-                        placeholderTextColor={placeholderTextColor}
-                        placeholder='Port'
-                        onChangeText={this.handlePortChange}
-                        value={port} />
-                    <Input
-                        style={styles.input}
-                        placeholderTextColor={placeholderTextColor}
-                        placeholder='Password (optional)' 
-                        onChangeText={this.handlePasswordChange} 
-                        value={password} 
-                        autoCapitalize='none'
-                        autoCompleteType='off'
-                        autoCorrect={false} />
-                    <View style={{marginVertical: 18}}>
-                        <Button
-                            title='Connect'
-                            onPress={this.handleSubmit}
-                            color={ThemeManager.instance().getCurrentTheme().activeColor}
-                        />
-                    </View>
-                </KeyboardAvoidingView>
+                 <MeasureLayout>
+                    {layout => (
+                        <KeyboardState layout={layout}>
+                            {keyboardInfo => (
+                                <KeyboardAwareLoginForm {...keyboardInfo}>
+                                    <View style={styles.credentialsContainer}>
+                                        <Image
+                                            source={require('../../assets/images/yamp_big_logo.png')}
+                                            style={{resizeMode: 'contain', width: imageHeight, height: imageHeight }} />
+                                        <Input
+                                            style={styles.input}
+                                            placeholderTextColor={placeholderTextColor}
+                                            placeholder='Host'
+                                            onChangeText={this.handleHostChange}
+                                            value={host} />
+                                        <Input
+                                            style={styles.input}
+                                            placeholderTextColor={placeholderTextColor}
+                                            placeholder='Port'
+                                            onChangeText={this.handlePortChange}
+                                            value={port} />
+                                        <Input
+                                            style={styles.input}
+                                            placeholderTextColor={placeholderTextColor}
+                                            placeholder='Password (optional)' 
+                                            onChangeText={this.handlePasswordChange} 
+                                            value={password} 
+                                            autoCapitalize='none'
+                                            autoCompleteType='off'
+                                            autoCorrect={false} />
+                                        <View style={{marginVertical: 18}}>
+                                            <Button
+                                                title='Connect'
+                                                onPress={this.handleSubmit}
+                                                color={ThemeManager.instance().getCurrentTheme().activeColor}
+                                            />
+                                        </View>
+                                    </View>
+                                </KeyboardAwareLoginForm>
+                            )}
+                        </KeyboardState>
+                    )}
+                </MeasureLayout>
                 <View style={styles.disclaimer}>
                     <Text style={{color: placeholderTextColor, textAlign: 'center', fontSize: 11}}>
                         © 2019{'\n'}Cover art powered by Last.fm
@@ -160,7 +220,7 @@ export default reduxConnect(mapStateToProps, mapDispatchToProps)(Login)
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: 'stretch',
         backgroundColor: ThemeManager.instance().getCurrentTheme().accentColor,
     },
     credentialsContainer: {
@@ -186,5 +246,6 @@ const styles = StyleSheet.create({
     input: {
         color:ThemeManager.instance().getCurrentTheme().activeColor,
         flex: 1,
+        width: '100%',
     }
 })
