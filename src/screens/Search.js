6012da6520354ed3620ab9839fb1bd09c4126dab
cog1to <sorryforbadname@gmail.com>
2019-10-05 17:01:25 +0300

    Fixed search results.

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {
-    View,
-    ScrollView,
-    Button,
-    StyleSheet,
-    Platform,
-    SafeAreaView,
+  View,
+  ScrollView,
+  Button,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
 } from 'react-native'
 import PropTypes from 'prop-types'
 import { NavigationActions } from 'react-navigation'
@@ -29,193 +29,197 @@ import ThemeManager from '../themes/ThemeManager'
 import { isIphoneX } from '../utils/IsIphoneX';
 
 Fields = [
-    { ID: 'TITLE', title: 'Title', tag: 'title', },
-    { ID: 'ARTIST', title: 'Artist', tag: 'artist', },
-    { ID: 'ALBUM', title: 'Album', tag: 'album', },
-    { ID: 'ALBUM_ARTIST', title: 'Album Artist', tag: 'albumartist', },
-    { ID: 'YEAR', title: 'Year', tag: 'date', },
-    { ID: 'GENRE', title: 'Genre', tag: 'genre', },
-    { ID: 'FILENAME', title: 'Filename', tag: 'filename', },
+  { ID: 'TITLE', title: 'Title', tag: 'title', },
+  { ID: 'ARTIST', title: 'Artist', tag: 'artist', },
+  { ID: 'ALBUM', title: 'Album', tag: 'album', },
+  { ID: 'ALBUM_ARTIST', title: 'Album Artist', tag: 'albumartist', },
+  { ID: 'YEAR', title: 'Year', tag: 'date', },
+  { ID: 'GENRE', title: 'Genre', tag: 'genre', },
+  { ID: 'FILENAME', title: 'Filename', tag: 'filename', },
 ]
 
 class KeyboardAwareSearchForm extends React.Component {
-    state = {
-        layout: null,
+  state = {
+    layout: null,
+  }
+
+  static propTypes = {
+    // From `KeyboardState`
+    keyboardHeight: PropTypes.number.isRequired,
+    keyboardVisible: PropTypes.bool.isRequired,
+    keyboardWillShow: PropTypes.bool.isRequired,
+    keyboardWillHide: PropTypes.bool.isRequired,
+    keyboardAnimationDuration: PropTypes.number.isRequired,
+    screenY: PropTypes.number.isRequired,
+
+    // Rendering content
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: null,
+  }
+
+  handleLayout = event => {
+    const { nativeEvent: { layout } } = event
+
+    if (this.state.layout == null) {
+      this.setState({
+        layout,
+      })
     }
+  }
 
-    static propTypes = {
-        // From `KeyboardState`
-        keyboardHeight: PropTypes.number.isRequired,
-        keyboardVisible: PropTypes.bool.isRequired,
-        keyboardWillShow: PropTypes.bool.isRequired,
-        keyboardWillHide: PropTypes.bool.isRequired,
-        keyboardAnimationDuration: PropTypes.number.isRequired,
-        screenY: PropTypes.number.isRequired,
+  render() {
+    const { layout } = this.state
 
-        // Rendering content
-        children: PropTypes.node,
-    }
+    const {
+      children,
+      keyboardHeight,
+      keyboardVisible,
+      keyboardAnimationDuration,
+      keyboardWillShow,
+      keyboardWillHide,
+      screenY,
+    } = this.props
 
-    static defaultProps = {
-        children: null,
-    }
+    console.log(keyboardVisible)
 
-    handleLayout = event => {
-        const { nativeEvent: { layout } } = event
-
-        if (this.state.layout == null) {
-            this.setState({
-                layout,
-            })
-        }
-    }
-
-    render() {
-        const { layout } = this.state
-
-        const {
-            children,
-            keyboardHeight,
-            keyboardVisible,
-            keyboardAnimationDuration,
-            keyboardWillShow,
-            keyboardWillHide,
-            screenY,
-        } = this.props
-
-        console.log(keyboardVisible)
-
-        const containerStyle = (layout != null && Platform.OS === 'ios')
-            ? { height: keyboardVisible ? (screenY - layout.y - 60) : layout.height } 
-            : { }
-        
-        return (
-            <View style={containerStyle} onLayout={this.handleLayout}>
-                {children}
-            </View>
-        )
-    }
+    const containerStyle = (layout != null && Platform.OS === 'ios')
+      ? { height: keyboardVisible ? (screenY - layout.y - 60) : layout.height } 
+      : { }
+    
+    return (
+      <View style={containerStyle} onLayout={this.handleLayout}>
+        {children}
+      </View>
+    )
+  }
 }
 
 class Search extends React.Component {
-    state = { 
-        dirty: false,
-        criteria: {},
+  state = { 
+    dirty: false,
+    criteria: {},
+  }
+
+  showResults = (content) => {
+    const { navigation } = this.props
+
+    content.forEach(item => {
+      item.status = 'none'
+    })
+
+    const action = NavigationActions.navigate({
+      params: {
+        content: content,
+      },
+      routeName: 'SearchResults',
+    })
+    navigation.dispatch(action)
+  }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    const { results } = nextProps
+
+    if (!nextState.dirty && results != null) {
+      this.showResults(results)
+      return false
     }
 
-    showResults = (content) => {
-        const { navigation } = this.props
+    return true
+  }
 
-        const action = NavigationActions.navigate({
-            params: {
-                content: content,
-            },
-            routeName: 'SearchResults',
-        })
-        navigation.dispatch(action)
-    }
+  onChangeText = (text, fieldId) => {
+    let newCriteria = Object.assign({}, this.state.criteria)
+    newCriteria[fieldId] = text
+    this.setState({
+      criteria: newCriteria,
+      dirty: true,
+    })
+  }
+
+  onSearch = () => {
+    const { doSearch } = this.props
+    const tags = this.state.criteria
+
+    const nonEmptyTags = Object.keys(tags).filter((key) => {
+      return (key in tags) && tags[key] !== null && tags[key].length > 0
+    })
+
+    const total = nonEmptyTags.map(tag => {
+      return { tag: tag, value: tags[tag] }
+    })
     
-    shouldComponentUpdate(nextProps, nextState) {
-        const { results } = nextProps
+    this.setState({
+      dirty: false,
+    }, () => doSearch(total))
+  }
 
-        if (!nextState.dirty && results != null) {
-            this.showResults(results)
-            return false
-        }
+  render() {
+    const { criteria } = this.state
+    const borderBottomColor = ThemeManager.instance().getCurrentTheme().accentColor
 
-        return true
-    }
-
-    onChangeText = (text, fieldId) => {
-        let newCriteria = Object.assign({}, this.state.criteria)
-        newCriteria[fieldId] = text
-        this.setState({
-            criteria: newCriteria,
-            dirty: true,
-        })
-    }
-
-    onSearch = () => {
-        const { doSearch } = this.props
-        const tags = this.state.criteria
-
-        const nonEmptyTags = Object.keys(tags).filter((key) => {
-            return (key in tags) && tags[key] !== null && tags[key].length > 0
-        })
-
-        const total = nonEmptyTags.map(tag => {
-            return { tag: tag, value: tags[tag] }
-        })
-        
-        this.setState({
-            dirty: false,
-        }, () => doSearch(total))
-    }
-
-    render() {
-        const { criteria } = this.state
-        const borderBottomColor = ThemeManager.instance().getCurrentTheme().accentColor
-
-        return (
-            <SafeAreaView style={styles.container}>
-                <KeyboardState>
-                    {keyboardInfo => (
-                        <KeyboardAwareSearchForm {...keyboardInfo}>
-                            <ScrollView keyboardShouldPersistTaps='always'>
-                                {Fields.map(({ ID, title, tag }) => {
-                                    return (
-                                        <Input
-                                            borderBottomColor={borderBottomColor}
-                                            key={ID}
-                                            placeholder={title}
-                                            onChangeText={(text) => this.onChangeText(text, tag)}
-                                            value={criteria[tag]}
-                                        />
-                                    )
-                                })}
-                               <View style={styles.search}>
-                                    <Button 
-                                        title="Search"
-                                        onPress={this.onSearch}
-                                        color={ThemeManager.instance().getCurrentTheme().accentColor}
-                                    />
-                                </View>
-                            </ScrollView>
-                        </KeyboardAwareSearchForm>
-                    )}
-                </KeyboardState>
-            </SafeAreaView>
-        )
-    }
+    return (
+      <SafeAreaView style={styles.container}>
+        <KeyboardState>
+          {keyboardInfo => (
+            <KeyboardAwareSearchForm {...keyboardInfo}>
+              <ScrollView keyboardShouldPersistTaps='always'>
+                {Fields.map(({ ID, title, tag }) => {
+                  return (
+                    <Input
+                      borderBottomColor={borderBottomColor}
+                      key={ID}
+                      placeholder={title}
+                      onChangeText={(text) => this.onChangeText(text, tag)}
+                      value={criteria[tag]}
+                    />
+                  )
+                })}
+                <View style={styles.search}>
+                  <Button 
+                    title="Search"
+                    onPress={this.onSearch}
+                    color={ThemeManager.instance().getCurrentTheme().accentColor}
+                  />
+                </View>
+              </ScrollView>
+            </KeyboardAwareSearchForm>
+          )}
+        </KeyboardState>
+      </SafeAreaView>
+    )
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        doSearch: (expression) => {
-            dispatch(search(expression))
-        }
+  return {
+    doSearch: (expression) => {
+      dispatch(search(expression))
     }
+  }
 }
 
 const mapStateToProps = (state) => {
-    const results = state.search
+  const results = state.search
 
-    return {
-        results: results,
-    }
+  return {
+    results: results,
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search)
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 5,
-        paddingHorizontal: 20,
-    },
-    search: {
-        marginTop: 10,
-        alignSelf: 'flex-end',
-    },
+  container: {
+    flex: 1,
+    paddingTop: 5,
+    paddingHorizontal: 20,
+  },
+  search: {
+    marginTop: 10,
+    alignSelf: 'flex-end',
+  },
 })
 
--- src/screens/SearchResults.js
+++ src/screens/SearchResults.js
@@ -10,9 +10,9 @@ import { connect } from 'react-redux'
 // Items list.
 import Browsable from '../components/common/Browsable'
 
-export default class SearchResults extends React.Component {
+class SearchResults extends React.Component {
   render() {
-    const { navigation } = this.props
+    const { navigation, queueSize, position } = this.props
     const { state: { params: { content } } } = navigation
 
     return (
@@ -23,11 +23,24 @@ export default class SearchResults extends React.Component {
           canDelete={false}
           canEdit={true}
           canArrange={false}
+          queueSize={queueSize}
+          position={position}
         />
       </View>
     )
   }
 }
+
+const mapStateToProps = state => {
+  const { position = null } = state.currentSong
+
+  return {
+    queueSize: state.queue.length,
+    position: position
+  }
+}
+
+export default connect(mapStateToProps)(SearchResults)
 
 const styles = StyleSheet.create({
   container: {
